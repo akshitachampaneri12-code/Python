a090e12619121890af4a1e241651c8ad31b36dd6
akshitachampaneri12-code <akshitachampaneri12@gmail.com>
2026-07-18 01:13:52 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+a=int(input("enter value of a:"))
+b=int(input("enter value of b:"))
+
+operator=input("enter operator: ")
+
+if operator=="+":
+    print(a+b)
+elif operator=="-":
+    print(a-b)
+elif operator=="*":
+    print(a*b)
+elif operator=="/":
+    print(a/b)
